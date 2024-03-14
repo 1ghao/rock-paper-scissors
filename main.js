89dd2a83aa75488fe0d32c51513f9391562218f0
1ghao <linwanghao2007@gmail.com>
2024-03-14 19:40:00 +0100

    add scoreboard and reset function

--- main.js
+++ main.js
@@ -17,20 +17,26 @@ function playRound(playerSelection, computerSelection) {
     return "It's a tie!";
   } else if (playerSelection === "rock") {
     if (computerSelection === "scissors") {
+      scoreCurrent += 1;
       return "You win!";
     } else {
+      scoreCurrent -= 1;
       return "You lost :(";
     }
   } else if (playerSelection === "paper") {
     if (computerSelection === "rock") {
+      scoreCurrent += 1;
       return "You win!";
     } else {
+      scoreCurrent -= 1;
       return "You lost :(";
     }
   } else if (playerSelection === "scissors") {
     if (computerSelection === "paper") {
+      scoreCurrent += 1;
       return "You win!";
     } else {
+      scoreCurrent -= 1;
       return "You lost :(";
     }
   } else {
@@ -46,8 +52,39 @@ function playGame() {
 const buttons = document.querySelector("#buttons");
 const result = document.querySelector("#result");
 
+const scoreboard = document.querySelector("#score");
+let scoreCurrent = 0;
+
 buttons.addEventListener("click", (e) => {
   let target = e.target;
 
   result.textContent = playRound(target.textContent, getComputerChoice());
+
+  scoreboard.textContent = scoreCurrent;
+
+  if (scoreCurrent == 5) {
+    result.textContent = "You won the game!!!";
+    resetGame();
+  } else if (scoreCurrent == -5) {
+    result.textContent = "You lost the game :(";
+    resetGame();
+  }
 });
+
+function resetGame() {
+  scoreCurrent = 0;
+  buttons.setAttribute("hidden", "hidden");
+
+  let restartButton = document.createElement("button");
+  restartButton.textContent = "New Game";
+  let body = document.querySelector("body");
+  body.appendChild(restartButton);
+
+  restartButton.addEventListener("click", () => {
+    buttons.removeAttribute("hidden");
+    body.removeChild(restartButton);
+
+    result.textContent = "Click to start";
+    scoreboard.textContent = scoreCurrent;
+  });
+}
